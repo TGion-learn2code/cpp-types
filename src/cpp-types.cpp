f50861da40ca883d38e8028a5f88f728bc8ca311
TGion <tobias@gion.io>
2020-07-20 11:50:34 +0200

    Project created

---
+++ src/cpp-types.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-types.cpp
+// Author      : Tobias Gion
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Types" << endl; // prints Types
+	return 0;
+}
